4047add5e80f0fd14ae2909162ae87c9e6b2bcd1
CHOI DONG OH <chlehddh8062@gmail.com>
2022-03-08 20:51:19 +0900

    220308 Test: controllerTest

---
+++ MarketWebBack/src/main/java/kr/green/sga/controller/UserController.java
@@ -0,0 +1,34 @@
+package kr.green.sga.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import kr.green.sga.service.UserService;
+import kr.green.sga.vo.TestVO;
+
+@RestController
+public class UserController {
+
+	@Autowired
+	private UserService userService;
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String rootGET() {
+		return "/";
+	}
+	
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public String rootPOST() {
+		return "/";
+	}
+	
+	@RequestMapping(value = "/test")
+	public String test(Model model) {
+		return userService.selectToday().toString();
+	}
+	
+	
+}
