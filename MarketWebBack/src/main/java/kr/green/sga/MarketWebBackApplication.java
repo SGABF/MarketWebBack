786c4a790db92966f27ec7b0dfc0e923bfacf0bc
CHOI DONG OH <chlehddh8062@gmail.com>
2022-03-08 12:41:47 +0900

    220308 Add: projectStart

---
+++ MarketWebBack/src/main/java/kr/green/sga/MarketWebBackApplication.java
@@ -0,0 +1,13 @@
+package kr.green.sga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketWebBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketWebBackApplication.class, args);
+	}
+
+}
---
+++ MarketWebBack/src/test/java/kr/green/sga/MarketWebBackApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.green.sga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketWebBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
