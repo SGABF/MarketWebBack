fe45bb2afd2af4eaede8d86964afccd72c399b5a
CHOI DONG OH <chlehddh8062@gmail.com>
2022-03-10 21:08:30 +0900

    220310 Feat: 기능 추가

--- MarketWebBack/src/main/java/kr/green/sga/service/UserService.java
+++ MarketWebBack/src/main/java/kr/green/sga/service/UserService.java
@@ -1,7 +1,5 @@
 package kr.green.sga.service;
 
-import java.util.Date;
-
 import kr.green.sga.vo.UserVO;
 
 public interface UserService {
@@ -17,5 +15,19 @@ public interface UserService {
 	// <!-- 4. delete_삭제하기(회원탈퇴하기) -->
 	void deleteUser(UserVO userVO);
 	
-
+	// ---------------------------------------------
+	// <!-- 7. select_아디 중복 확인(0:없음/사용가능 1:있음/사용불가) -->
+	// selectCountUserId 사용	(user_id)
+	int idCheck(String userid);
+	
+	// <!-- 8. select_아디 찾기 -->
+	// selectByUsername 사용	(user_name, user_phone)
+	UserVO findId(UserVO UserVO);
+	
+	// <!-- 9. select_비번 찾기 // ID와 전화번호로 가져오기 -->
+	// selectByUserId 사용		(user_id, user_phone)
+	UserVO findPw(UserVO UserVO);
+	
+	// <!-- 11. update_비밀번호 변경하기 -->
+	void updatePassword(UserVO userVO);
 }
