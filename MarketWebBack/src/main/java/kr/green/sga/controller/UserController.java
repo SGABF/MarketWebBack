1d5ba33095a7d92c597e2a0f76f3f2e9af00ecdb
CHOI DONG OH <chlehddh8062@gmail.com>
2022-03-11 02:54:27 +0900

    220311 Feat: BannedUser idCheck 추가

--- MarketWebBack/src/main/java/kr/green/sga/controller/UserController.java
+++ MarketWebBack/src/main/java/kr/green/sga/controller/UserController.java
@@ -1,25 +1,28 @@
 package kr.green.sga.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import kr.green.sga.dao.UserDAO;
 import kr.green.sga.service.UserService;
 import kr.green.sga.vo.UserVO;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestController
 public class UserController {
-	
+
 	@Autowired
 	private UserService userService;
 	
+	@Autowired
+	private UserDAO userDAO;
+
 	@RequestMapping(value = "/")
-	@ResponseBody
 	public String root() throws Exception {
 		UserVO userVO = null;
 		userService.insertUser(userVO);
@@ -27,78 +30,51 @@ public class UserController {
 		return "/";
 	}
 	
-	@RequestMapping(value = "/idCheck")
-	public int idCheck() {
-		String id = "chlehddh"; 
-		return userService.idCheck(id); 
+	@RequestMapping(value = "/idCheck", method = RequestMethod.GET)
+	public String idCheckGET(@RequestParam(required = false) String user_id) {
+		// 추후 업데이트 예정 "잘못된 접근입니다."
+		return "";
+	}
+
+	@RequestMapping(value = "/idCheck", method = RequestMethod.POST)
+	public String idCheckPOST(@RequestParam(required = false) String user_id) {
+		String userids[] = "chlehddh,admin,root,master,webmaster,administrator".split(","); // 금지 아이디 목록
+		int count = 0;
+		for (String id : userids) {
+			if (user_id.equals(id)) {
+				count = 1;
+				break;
+			}
+		}
+		if (count == 0) {
+			count = userService.idCheck(user_id);
+		}
+		return count + "";
 	}
 	
-	@RequestMapping(value = "/deleteUser")
-	public String deleteUser(@ModelAttribute UserVO userVO, Model model) {
-//		UserVO vo = userService.deleteUser(userVO.getUser);
-		System.out.println(userVO);
-//		model.addAttribute("vo", vo);
-		return "deleteUser";
+	@RequestMapping(value = "/BannedUser", method = RequestMethod.GET)
+	public String BannedUserGET(@RequestParam(required = false) int user_idx) {
+		// 추후 업데이트 예정 "잘못된 접근입니다."
+		return "";
 	}
 	
-	@RequestMapping(value = "/selectCountUserId")
-	@ResponseBody
-	public String selectCountUserId() {
-		UserVO userVO = null;
-		userService.insertUser(userVO);
-		System.out.println(userVO);
-		return "selectCountUserId";
+	@RequestMapping(value = "/BannedUser", method = RequestMethod.POST)
+	public String BannedUserPOST(@RequestParam(required = false) int user_idx) {
+		UserVO userVO = userDAO.selectByIdx(user_idx);
+		userService.BannedUser(userVO);
+		return userVO.toString();
 	}
-	
-	@RequestMapping(value = "/selectByUsername")
-	@ResponseBody
-	public String selectByUsername() {
-		UserVO userVO = null;
-		userService.insertUser(userVO);
-		System.out.println(userVO);
-		return "selectByIdx";
-	}
-	
-	@RequestMapping(value = "/selectByUserId")
-	@ResponseBody
-	public String selectByUserId() {
-		UserVO userVO = null;
-		userService.insertUser(userVO);
-		System.out.println(userVO);
-		return "selectByIdx";
-	}
-	
-	@RequestMapping(value = "/updatePassword")
-	@ResponseBody
-	public String updatePassword() {
-		UserVO userVO = null;
-		userService.insertUser(userVO);
-		System.out.println(userVO);
-		return "selectByIdx";
-	}
-	
-	@RequestMapping(value = "/selectUserId")
-	@ResponseBody
-	public String selectUserId() {
-		UserVO userVO = null;
-		userService.insertUser(userVO);
-		System.out.println(userVO);
-		return "selectUserId";
-	}   
 
 	@RequestMapping(value = "/get", method = RequestMethod.GET)
-	@ResponseBody
 	public String rootGET() {
 		System.out.println("get");
 		return "get";
 	}
 
 	@RequestMapping(value = "/post", method = RequestMethod.POST)
-	@ResponseBody
 	public String rootPOST() {
 		System.out.println("post");
 		return "post";
 	}
-	
 
 }
