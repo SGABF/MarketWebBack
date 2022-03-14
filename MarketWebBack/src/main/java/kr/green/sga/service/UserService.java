26c109db4cf9f11835ffeb606d5e92000f3cc639
CHOI DONG OH <chlehddh8062@gmail.com>
2022-03-14 22:41:52 +0900

    220314 Update: Service

--- MarketWebBack/src/main/java/kr/green/sga/service/UserService.java
+++ MarketWebBack/src/main/java/kr/green/sga/service/UserService.java
@@ -1,34 +1,35 @@
 package kr.green.sga.service;
 
-import java.util.HashMap;
-
 import kr.green.sga.vo.UserVO;
 
 public interface UserService {
-	// <!-- 1. insert_저장하기(회원가입하기) -->
+	// <!-- 01. insert_저장하기(회원가입하기) -->
 	void insertUser(UserVO userVO);
 	
-	// <!-- 2. select_1개 얻기 -->
+	// <!-- 02. select_1개 얻기 -->
 	UserVO selectByIdx(int user_idx);
 	
-	// <!-- 3. update_수정하기(회원정보수정하기) -->
+	// <!-- 03. update_수정하기(회원정보수정하기) -->
 	void updateUser(UserVO userVO);
 	
-	// <!-- 4. delete_삭제하기(회원탈퇴하기) -->
+	// <!-- 04. delete_삭제하기(회원탈퇴하기) -->
 	void deleteUser(UserVO userVO);
 	
+// -----------------------------------------------------------------
+	
+	// <!-- 05. select_아디 찾기 -->
+	// selectByUserNameEmail 사용		(user_name, user_email)
+	String findId(String user_name, String user_email);
+	
+	// <!-- 06. select_비번 찾기 -->
+	// selectByUserIdNameEmail 사용		(user_id, user_email, user_name)
+	String findPw(String user_id, String user_email, String user_name);
+	
 	// ---------------------------------------------
-	// <!-- 7. select_아디 중복 확인(0:없음/사용가능 1:있음/사용불가) -->
+	
+	// <!-- 07. select_아디 중복 확인(0:없음/사용가능 1:있음/사용불가) -->
 	// selectCountUserId 사용	(user_id)
 	int idCheck(String user_id);
-	
-	// <!-- 8. select_아디 찾기 -->
-	// selectByUsername 사용	(user_name, user_phone)
-	UserVO findId(UserVO UserVO);
-	
-	// <!-- 9. select_비번 찾기 // ID와 전화번호로 가져오기 -->
-	// selectByUserId 사용		(user_id, user_phone)
-	UserVO findPw(UserVO UserVO);
 	
 	// <!-- 10. update_유저 벤 하기 -->
 	// Hashmap 사용 			(user_banned, user_idx)
@@ -37,15 +38,18 @@ public interface UserService {
 	// <!-- 11. update_비밀번호 변경하기 -->
 	void updatePassword(UserVO userVO);
 	
+	// <!-- 12. String_임시 비밀번호 생성하기 -->
+	String makePassword(int length);
+	
 	// ---------------------------------------------
-	// <!-- 12. ID로 가져오기 -->
-	UserVO selectUserId(String userid);
-
-	// <!-- 12. select_이름과 전화번호로 가져오기(아디찾기 사용) -->
+	
+	// <!-- 50. select_이름과 전화번호로 가져오기(아디찾기 사용) -->
 	UserVO selectByUsername(UserVO userVO);
 	
-	// <!-- 13. select_아디와 전화번호로 가져오기(비번찾기 사용) -->
+	// <!-- 51. select_아디와 전화번호로 가져오기(비번찾기 사용) -->
 	UserVO selectByUserId(UserVO userVO);
 	
+	// <!-- 52. ID로 VO 가져오기 -->
+	UserVO selectUserId(String user_id);
 
 }
--- MarketWebBack/src/main/java/kr/green/sga/service/UserServiceImpl.java
+++ MarketWebBack/src/main/java/kr/green/sga/service/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package kr.green.sga.service;
 
 import java.util.HashMap;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -21,7 +22,7 @@ public class UserServiceImpl implements UserService {
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	@Override
-	// <!-- 1. insert_저장하기(회원가입하기) -->
+	// <!-- 01. insert_저장하기(회원가입하기) -->
 	public void insertUser(UserVO userVO) {
 		log.info("UserServiceImpl-insertUser 호출 : " + userVO);
 		// userVO에 넘겨 받은 값이 있다면
@@ -32,7 +33,19 @@ public class UserServiceImpl implements UserService {
 	}
 
 	@Override
-	// <!-- 3. update_수정하기(회원정보수정하기) -->
+	// <!-- 02. select_1개 얻기 -->
+	public UserVO selectByIdx(int user_idx) {
+		log.info("UserServiceImpl-selectByIdx 호출 : " + user_idx);
+		UserVO userVO = null;
+		if (user_idx != 0) {
+			userVO = userDAO.selectByIdx(user_idx);
+		}
+		log.info("UserServiceImpl-selectByIdx 리턴 : " + userVO);
+		return userVO;
+	}
+
+	@Override
+	// <!-- 03. update_수정하기(회원정보수정하기) -->
 	public void updateUser(UserVO userVO) {
 		log.info("UserServiceImpl-updateUser 호출 : " + userVO);
 		if (userVO != null) {
@@ -52,62 +65,66 @@ public class UserServiceImpl implements UserService {
 	}
 
 	@Override
-	// <!-- 4. delete_삭제하기(회원탈퇴하기) -->
+	// <!-- 04. delete_삭제하기(회원탈퇴하기) -->
 	public void deleteUser(UserVO userVO) {
 		log.info("UserServiceImpl-deleteUser 호출 : " + userVO);
+		UserVO dbVO = null;
 		// 넘겨받은 userVO가 있다면
 		if (userVO != null) {
 			// 아이디 정보를 담아서 vo에 넣기
-			UserVO vo = userDAO.selectUserId(userVO.getUser_id());
-			if (vo != null) {
+			dbVO = userDAO.selectByIdx(userVO.getUser_idx());
+			if (dbVO != null) {
 				// 암호화된 내용을 db에서 가져옴
-				String dbPassword = vo.getUser_password();
+				String dbPassword = dbVO.getUser_password();
 				if (bCryptPasswordEncoder.matches(userVO.getUser_password(), dbPassword)) {
+					log.info("UserServiceImpl-deleteUser-bCryptPasswordEncoder.matches 호출 및 비번 일치확인");
 					// 회원탈퇴
-					userDAO.deleteUser(vo.getUser_idx());
+					userDAO.deleteUser(dbVO.getUser_idx());
+					log.info("UserServiceImpl-deleteUser 회원정보삭제됨 : " + dbVO);
 				}
 			}
 		}
 	}
 
 	@Override
-	// <!-- 7. select_아디 중복 확인(0:없음/사용가능 1:있음/사용불가) -->
+	// <!-- 07. select_아디 중복 확인(0:없음/사용가능 1:있음/사용불가) -->
 	// selectCountUserId 사용
 	public int idCheck(String user_id) {
-		log.info("ServiceImpl_idCheck 호출 : " + user_id);
-		return userDAO.selectCountUserId(user_id);
+		int count = 0;
+		if (user_id != null) {
+			log.info("ServiceImpl_idCheck 호출 : " + user_id);
+			count = userDAO.selectCountUserId(user_id);
+			log.info("ServiceImpl_idCheck 리턴 : " + count);
+		}
+		return count;
 	}
 
 	@Override
-	// <!-- 8. select_아디 찾기 // ID와 전화번호로 가져오기 -->
-	// selectByUsername 사용
-	public UserVO findId(UserVO userVO) {
-		log.info("UserServiceImpl-findId 호출 : " + userVO);
-		UserVO vo = null;
-		if(userVO!=null) {
-			HashMap<String, String> map = new HashMap<String, String>();
-			map.put("username", userVO.getUser_name());
-			map.put("phone", userVO.getUser_phone());
-			vo = userDAO.selectByUsername(map);
+	// <!-- 08. select_아디 찾기 // 이름과 이메일로 가져오기 -->
+	// 53. selectByUserNameEmail 사용
+	public String findId(String user_name, String user_email) {
+		log.info("UserServiceImpl-findId 호출 : " + user_name + ", " + user_email);
+		UserVO dbVO = null;
+		String user_id = "";
+		if (user_name != null && user_email != null) {
+			dbVO = userDAO.selectByUserNameEmail(user_name, user_email);
+			user_id = dbVO.getUser_id();
 		}
-		log.info("UserServiceImpl-findId 리턴 : " + vo);
-		return vo;
+		log.info("UserServiceImpl-findId 리턴 : " + user_id);
+		return user_id;
 	}
 
 	@Override
-	// <!-- 9. select_비번 찾기 // ID와 전화번호로 가져오기 -->
-	// selectByUserId 사용
-	public UserVO findPw(UserVO userVO) {
-		log.info("UserServiceImpl-findPw 호출 : " + userVO);
-		UserVO vo = null;
-		if(userVO!=null) {
-			HashMap<String, String> map = new HashMap<String, String>();
-			map.put("userid", userVO.getUser_id());
-			map.put("phone", userVO.getUser_phone());
-			vo = userDAO.selectByUserId(map);
+	// <!-- 09. select_비번 찾기 // ID와 전화번호로 가져오기 -->
+	// 54. selectByUserIdNameEmail 사용
+	public String findPw(String user_id, String user_email, String user_name) {
+		log.info("UserServiceImpl-findPw 호출 : " + user_id + ", " + user_email+ ", " + user_name);
+		UserVO dbVO = null;
+		if (user_id != null && user_email != null && user_name != null) {
+			dbVO = userDAO.selectByUserIdNameEmail(user_id, user_email, user_name);
 		}
-		log.info("UserServiceImpl-findPw 리턴 : " + vo);
-		return vo;
+		log.info("UserServiceImpl-findPw 리턴 : " + dbVO.getUser_password());
+		return dbVO.getUser_password();
 	}
 
 	@Override
@@ -115,24 +132,27 @@ public class UserServiceImpl implements UserService {
 	public void updatePassword(UserVO userVO) {
 		log.info("UserServiceImpl-updatePassword 호출 : " + userVO);
 		HashMap<String, String> map = new HashMap<String, String>();
-		map.put("userid", userVO.getUser_id());	
-		// password는 암호화해서 저장되어야 한다.
-		String encryptPassword = bCryptPasswordEncoder.encode(userVO.getUser_password());
-		map.put("password", encryptPassword);		
-		userDAO.updatePassword(map);
+		if (userVO != null) {
+			map.put("user_id", userVO.getUser_id());
+			// password는 암호화해서 저장되어야 한다.
+			String encryptPassword = bCryptPasswordEncoder.encode(userVO.getUser_password());
+			map.put("user_password", encryptPassword);
+			userDAO.updatePassword(map);
+		}
+		log.info("UserServiceImpl-updatePassword 리턴 : " + map);
 	}
 
 	@Override
 	// <!-- 10. update_유저 벤 하기 -->
-	// Hashmap 사용 			(user_banned, user_idx)
+	// Hashmap 사용 (user_banned, user_idx)
 	public void BannedUser(UserVO userVO) {
 		log.info("UserServiceImpl-BannedUser 호출 : " + userVO);
 		HashMap<String, Integer> map = new HashMap<String, Integer>();
-		if(userVO.getUser_banned()==0) {
+		if (userVO.getUser_banned() == 0) {
 			map.put("user_banned", 1);
 			map.put("user_idx", userVO.getUser_idx());
 			userDAO.BannedUser(map);
-		}else {
+		} else {
 			map.put("user_banned", 0);
 			map.put("user_idx", userVO.getUser_idx());
 			userDAO.BannedUser(map);
@@ -141,23 +161,11 @@ public class UserServiceImpl implements UserService {
 	}
 
 	@Override
-	// <!-- 2. select_1개 얻기 -->
-	public UserVO selectByIdx(int user_idx) {
-		log.info("UserServiceImpl-selectByIdx 호출 : " + user_idx);
-		UserVO userVO = null;
-		if(user_idx!=0) {
-			userVO = userDAO.selectByIdx(user_idx);
-		}
-		log.info("UserServiceImpl-selectByIdx 리턴 : " + userVO);
-		return userVO;
-	}
-
-	@Override
-	// <!-- 12. select_이름과 전화번호로 가져오기(아디찾기 사용) -->
+	// <!-- 8. select_이름과 전화번호로 가져오기(아디찾기 사용) -->
 	public UserVO selectByUsername(UserVO userVO) {
 		log.info("UserServiceImpl-selectByUsername 호출 : " + userVO);
 		UserVO vo = null;
-		if(userVO != null) {
+		if (userVO != null) {
 			HashMap<String, String> map = new HashMap<String, String>();
 			map.put("user_name", userVO.getUser_name());
 			map.put("user_phone", userVO.getUser_phone());
@@ -168,11 +176,11 @@ public class UserServiceImpl implements UserService {
 	}
 
 	@Override
-	// <!-- 13. select_아디와 전화번호로 가져오기(비번찾기 사용) -->
+	// <!-- 09. select_아디와 전화번호로 가져오기(비번찾기 사용) -->
 	public UserVO selectByUserId(UserVO userVO) {
 		log.info("UserServiceImpl-selectByUserId 호출 : " + userVO);
 		UserVO vo = null;
-		if(userVO != null) {
+		if (userVO != null) {
 			HashMap<String, String> map = new HashMap<String, String>();
 			map.put("user_name", userVO.getUser_id());
 			map.put("user_phone", userVO.getUser_phone());
@@ -183,8 +191,43 @@ public class UserServiceImpl implements UserService {
 	}
 
 	@Override
-	public UserVO selectUserId(String userid) {
-		// TODO Auto-generated method stub
+	// <!-- 50. ID로 가져오기 -->
+	public UserVO selectUserId(String user_id) {
+		log.info("UserServiceImpl-selectUserId 호출 : " + user_id);
+		UserVO userVO = null;
+		if (user_id != null) {
+			userVO = userDAO.selectUserId(user_id);
+		}
+		if (userVO != null) {
+			log.info("UserServiceImpl-selectUserId 리턴 : " + userVO);
+		}
 		return null;
 	}
+	
+	
+	// 임시비밀번호를 만들어주는 메서드
+		public String makePassword(int length) {
+			Random random = new Random();
+			String password="";
+			String str="~@!#$%^&*+-*";
+			for(int i=0;i<length;i++) {
+				// case의 개수가 많을수록 나타날 확율이 높아진다.
+				switch (random.nextInt(8)) { // 0(숫자), 1(영어소문자), 2(영어 대문자), 3(특수문자)
+				case 0: case 18: case 19:
+					password += (char)('0' + random.nextInt(10));
+					break;
+				case 1: case 4: case 5: case 6: case 7: case 8: case 9: case 10:
+					password += (char)('a' + random.nextInt(26));
+					break;
+				case 2: case 11: case 12: case 13: case 14: case 15: case 16: case 17:
+					password += (char)('A' + random.nextInt(26));
+					break;
+				case 3:
+					password += str.charAt(random.nextInt(str.length()));
+					break;
+				}
+			}
+			return password;
+		}
+		
 }
