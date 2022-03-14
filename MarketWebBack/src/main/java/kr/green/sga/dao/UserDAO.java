2df18de1eeefa937e7c13c2160169d72ac58b307
CHOI DONG OH <chlehddh8062@gmail.com>
2022-03-14 22:41:37 +0900

    220314 Update: DAO

--- MarketWebBack/src/main/java/kr/green/sga/dao/UserDAO.java
+++ MarketWebBack/src/main/java/kr/green/sga/dao/UserDAO.java
@@ -8,25 +8,27 @@ import kr.green.sga.vo.UserVO;
 
 @Mapper
 public interface UserDAO {
-	// <!-- 1. insert_저장하기(회원가입하기) -->
+	// <!-- 01. insert_저장하기(회원가입하기) -->
 	void insertUser(UserVO userVO);
 
-	// <!-- 2. select_1개 얻기 -->
-	UserVO selectByIdx(int idx);
+	// <!-- 02. select_1개 얻기 -->
+	UserVO selectByIdx(int user_idx);
 
-	// <!-- 3. update_수정하기(회원정보수정하기) -->
+	// <!-- 03. update_수정하기(회원정보수정하기) -->
 	void updateUser(UserVO userVO);
 
-	// <!-- 4. delete_삭제하기(회원탈퇴하기) -->
-	void deleteUser(int idx);
-
-	// <!-- 7. select_아디 중복 확인(0:없음/사용가능 1:있음/사용불가)-->
-	int selectCountUserId(String userid);
+	// <!-- 04. delete_삭제하기(회원탈퇴하기) -->
+	void deleteUser(int user_idx);
 	
-	// <!-- 8. select_이름과 전화번호로 가져오기(아디찾기 사용) -->
+// -----------------------------------------------------------------
+	
+	// <!-- 07. select_아디 중복 확인(0:없음/사용가능 1:있음/사용불가)-->
+	int selectCountUserId(String user_id);
+	
+	// <!-- 08. select_이름과 전화번호로 가져오기(아디찾기 사용) -->
 	UserVO selectByUsername(HashMap<String, String> map);
 
-	// <!-- 9. select_아디와 전화번호로 가져오기(비번찾기 사용) -->
+	// <!-- 09. select_아디와 전화번호로 가져오기(비번찾기 사용) -->
 	UserVO selectByUserId(HashMap<String, String> map);
 
 	// <!-- 10. update_유저 벤 하기 -->
@@ -34,9 +36,21 @@ public interface UserDAO {
 	
 	// <!-- 11. update_비밀번호 변경하기 -->
 	void updatePassword(HashMap<String, String> map);
+	
+// -----------------------------------------------------------------
 
-	// <!-- 12. ID로 가져오기 -->
-	UserVO selectUserId(String userid);
+	// <!-- 52. ID로 VO 가져오기 -->
+	UserVO selectUserId(String user_id);
+
+	// <!-- 53. 이름, 전화번호로 VO 가져오기 -->	
+	// findId
+	UserVO selectByUserNameEmail(String user_name, String user_email);
+	
+	// <!-- 54. 아디, 전화번호로 VO 가져오기 -->	
+	// findPw
+	UserVO selectByUserIdNameEmail(String user_id, String user_email, String user_name);
+
+
 
 
 }
